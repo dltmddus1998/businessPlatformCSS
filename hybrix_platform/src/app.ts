05dc4ee59eb9879d17b3cde5a8f959b9386ebcf5
이승연 <sylee1274@mz.co.kr>
2023-02-24 16:52:18 +0900

    Feat: b2c marketplace

---
+++ hybrix_platform/src/app.ts
@@ -0,0 +1,8 @@
+// import { Component } from "./components/component.js";
+
+// class App {
+//   private readonly console: Component & Composable
+//   constructor() {}
+// }
+
+// // new App(document.querySelector('.document')! as HTMLElement, document.body);
